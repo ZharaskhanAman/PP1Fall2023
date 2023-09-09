9b658ffe8f2a15be22d26ab453ac581cdc32d546
Zharaskhan Aman <aman.zharaskhan@gmail.com>
2023-09-09 12:10:52 +0600

    Move hello.cpp to the lecture/week1

---
+++ lecture/week1/hello.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+// this is single line comment
+/*
+    This is multi line comment
+    This is the next line
+*/
+
+// return_type function_name() {}
+int main() {
+    cout << "Привет!Hello KBTU!";
+
+    return 0;
+}
+
+// Compiler 
+// g++ filename.cpp -o filename
+// g++ hello.cpp -o hello
