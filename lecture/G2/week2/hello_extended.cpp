1f39fe8296d7c647442a8f4cbf3b1893902f330c
Zharaskhan Aman <aman.zharaskhan@gmail.com>
2023-09-13 10:02:17 +0600

    Add lecture notes G2/week2

---
+++ lecture/G2/week2/hello_extended.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello KBTU!" << endl;
+    cout << "Hello KBTU!" << " " << "Hello KBTU!" << endl;
+    cout << "This is the quotation mark \" " << endl;
+    cout << "First line\n" << "Second line";
+   
+    return 0;
+} 
